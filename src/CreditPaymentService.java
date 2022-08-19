fd79f33a76c1eaae4466abf5aae576a0622efbe0
SergeyESh <SergeyESh1989@yandex.ru>
2022-08-19 14:57:35 +0300

    Fixed variable names

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
-    public int calculate(int amount, int term, double annual_rate) {
-        double monthly_rate;
-        monthly_rate = annual_rate / 12 / 100; // месячная процентная ставка
+    public int calculate(int amount, int term, double annualRate) {
+        double monthlyRate;
+        monthlyRate = annualRate / 12 / 100; // месячная процентная ставка
         double ratio; // коэфициент аннуитета
-        ratio = (monthly_rate * Math.pow((1 + monthly_rate), term)) / (Math.pow((1 + monthly_rate), term) - 1);
+        ratio = (monthlyRate * Math.pow((1 + monthlyRate), term)) / (Math.pow((1 + monthlyRate), term) - 1);
         int annuity;
         annuity = (int) (amount * ratio);
         return annuity;
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double annual_rate = 9.99; // годовая процентная ставка
-        int annuity_1 = service.calculate(1_000_000, 12, annual_rate);
-        System.out.println("Ежемесячный платёж: " + annuity_1 + " р.");
-        System.out.println("Процентная ставка: " + annual_rate + " %");
+        double annualRate = 9.99; // годовая процентная ставка
+        int annuity1 = service.calculate(1_000_000, 12, annualRate);
+        System.out.println("Ежемесячный платёж: " + annuity1 + " р.");
+        System.out.println("Процентная ставка: " + annualRate + " %");
 
         System.out.println();
-        int annuity_2 = service.calculate(1_000_000, 24, annual_rate);
-        System.out.println("Ежемесячный платёж: " + annuity_2 + " р.");
-        System.out.println("Процентная ставка: " + annual_rate + " %");
+        int annuity2 = service.calculate(1_000_000, 24, annualRate);
+        System.out.println("Ежемесячный платёж: " + annuity2 + " р.");
+        System.out.println("Процентная ставка: " + annualRate + " %");
 
         System.out.println();
-        int annuity_3 = service.calculate(1_000_000, 36, annual_rate);
-        System.out.println("Ежемесячный платёж: " + annuity_3 + " р.");
-        System.out.println("Процентная ставка: " + annual_rate + " %");
+        int annuity3 = service.calculate(1_000_000, 36, annualRate);
+        System.out.println("Ежемесячный платёж: " + annuity3 + " р.");
+        System.out.println("Процентная ставка: " + annualRate + " %");
     }
 }
